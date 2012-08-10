b3dc295ee732e8ed65dcae15e480bff037599c14
Liang Ding <DL88250@gmail.com>
2012-08-10 12:51:46 +0800

    @Y 试试

--- src/main/java/org/b3log/symphony/processor/LoginProcessor.java
+++ src/main/java/org/b3log/symphony/processor/LoginProcessor.java
@@ -111,6 +111,15 @@ public class LoginProcessor {
 
         final JSONObject requestJSONObject = Requests.parseRequestJSONObject(request, response);
         final String name = requestJSONObject.optString("name");
+
+        if (invalidUserName(name)) {
+            ret.put(Keys.MSG, langPropsService.get("registerFailLabel") + " - " + langPropsService.get("invalidUserNameLabel"));
+            return;
+        }
+        
+        
+
+
         final String email = requestJSONObject.optString("email");
         final String password = requestJSONObject.optString("password");
         final String captcha = requestJSONObject.optString("captcha");
@@ -174,5 +183,39 @@ public class LoginProcessor {
         } catch (final ServiceException e) {
             ret.put(Keys.MSG, langPropsService.get("loginFailLabel"));
         }
+    }
+
+    /**
+     * Checks whether the specified name is invalid.
+     * 
+     * <p>
+     * A valid user name:
+     *   <ul>
+     *     <li>length [1, 20]</li>
+     *     <li>content {a-z, A-Z, 0-9, _}</li>
+     *   </ul>
+     * </p>
+     * 
+     * @param name the specified name
+     * @return {@code true} if it is invalid, returns {@code false} otherwise
+     */
+    private boolean invalidUserName(final String name) {
+        final int length = name.length();
+        if (length < 1 || length > 20) {
+            return true;
+        }
+
+        char c = 0;
+        for (int i = 0; i < length; i++) {
+            c = name.charAt(i);
+
+            if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || '0' <= c && c <= '9' || '_' == c) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
