33b5cce84858a22347b6f4aaa90cf72ed22b0f40
Liang Ding <DL88250@gmail.com>
2013-09-05 09:58:52 +0800

    版本 0.2.5-DEV

--- src/main/java/org/b3log/symphony/SymphonyServletListener.java
+++ src/main/java/org/b3log/symphony/SymphonyServletListener.java
@@ -55,7 +55,7 @@ public final class SymphonyServletListener extends AbstractServletListener {
     /**
      * B3log Symphony version.
      */
-    public static final String VERSION = "0.2.1";
+    public static final String VERSION = "0.2.5-DEV";
     /**
      * Logger.
      */
