65aad116c2062ac4917b9e7c0ea503701c5c8015
Liang Ding <DL88250@gmail.com>
2014-01-05 14:15:50 +0000

    时区问题修复

--- src/main/java/org/b3log/symphony/util/TimeZones.java
+++ src/main/java/org/b3log/symphony/util/TimeZones.java
@@ -15,7 +15,6 @@
  */
 package org.b3log.symphony.util;
 
-import java.util.Date;
 import java.util.TimeZone;
 import org.b3log.latke.util.freemarker.Templates;
 
@@ -23,42 +22,19 @@ import org.b3log.latke.util.freemarker.Templates;
  * Time zone utilities.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.0.0, Jul 30, 2012
+ * @version 1.0.0.1, Jan 5, 2014
  * @since 0.2.0
  */
 public final class TimeZones {
 
     /**
-     * Gets the current date with the specified time zone id.
-     *
-     * @param timeZoneId the specified time zone id
-     * @return date
-     */
-    public static Date getTime(final String timeZoneId) {
-        final TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
-        final TimeZone defaultTimeZone = TimeZone.getDefault();
-        TimeZone.setDefault(timeZone);
-        final Date ret = new Date();
-        TimeZone.setDefault(defaultTimeZone);
-
-        return ret;
-    }
-
-    /**
      * Sets time zone by the specified time zone id.
-     *
-     * <p>
-     * This method will call {@linkplain TimeZone#setDefault(java.util.TimeZone)},
-     * and set time zone for all date formats and template configuration.
-     * </p>
      *
      * @param timeZoneId the specified time zone id
      */
     public static void setTimeZone(final String timeZoneId) {
         final TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
 
-        TimeZone.setDefault(timeZone);
-        System.setProperty("user.timezone", timeZoneId);
         Templates.MAIN_CFG.setTimeZone(timeZone);
     }
 
