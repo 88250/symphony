e5ceb73ad905fedf5608c7dc918031a4dd6e6469
wangdx <dongxu@apache.org>
2015-08-18 22:10:24 +0800

    finish the checkin using real data.

--- src/main/java/org/b3log/symphony/api/ActivityProcessor.java
+++ src/main/java/org/b3log/symphony/api/ActivityProcessor.java
@@ -15,13 +15,21 @@
  */
 package org.b3log.symphony.api;
 
+import java.io.IOException;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.b3log.latke.Keys;
+import org.b3log.latke.service.ServiceException;
 import org.b3log.latke.servlet.HTTPRequestContext;
 import org.b3log.latke.servlet.HTTPRequestMethod;
 import org.b3log.latke.servlet.annotation.RequestProcessing;
 import org.b3log.latke.servlet.annotation.RequestProcessor;
 import org.b3log.latke.servlet.renderer.JSONRenderer;
+import org.b3log.symphony.model.UserExt;
+import org.b3log.symphony.service.ActivityMgmtService;
+import org.b3log.symphony.service.UserQueryService;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -39,23 +47,60 @@ import org.json.JSONObject;
 public class ActivityProcessor {
     
     /**
-     * Mobile logins user.
+     * User query service.
+     */
+    @Inject
+    private UserQueryService userQueryService;
+
+    /**
+     * Activity management service.
+     */
+    @Inject
+    private ActivityMgmtService activityMgmtService;
+    
+    /**
+     * Checkin.
      *
      * @param context the specified context
      * @param request the specified request
      * @param response the specified response
+     * @throws JSONException json exception
+     * @throws IOException io exception
+     * @throws ServiceException service exception
      */
     @RequestProcessing(value = "/api/v1/activities/checkin", method = HTTPRequestMethod.POST)
-    public void checkin(final HTTPRequestContext context, final HttpServletRequest request, final HttpServletResponse response) {
+    public void checkin(final HTTPRequestContext context, final HttpServletRequest request, final HttpServletResponse response) 
+            throws JSONException, IOException, ServiceException {
+        final String auth = request.getHeader("Authorization");
+        if(auth == null){//TODO validate
+            return;
+        }
+        final String email = new JSONObject(auth.substring("Bearer ".length())).optString("userEmail");
+        
+        final JSONObject currentUser = userQueryService.getUserByEmail(email);
+        if (null == currentUser) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        
+        final String userId = currentUser.optString(Keys.OBJECT_ID);
+        final int checkinReward = activityMgmtService.dailyCheckin(userId);
+        final int balance = currentUser.optInt(UserExt.USER_POINT);
+        
+        activityMgmtService.fillCheckinStreak(currentUser);
+        
+        final int streak = currentUser.optInt(UserExt.USER_T_CURRENT_CHECKIN_STREAK);
+        final int streaked = currentUser.optInt(UserExt.USER_T_LONGEST_CHECKIN_STREAK);
+        
         final JSONRenderer renderer = new JSONRenderer();
         context.setRenderer(renderer);
         final JSONObject ret = new JSONObject();
         renderer.setJSONObject(ret);
         final JSONObject checkin = new JSONObject();
-        checkin.put("balance", 1905);
-        checkin.put("reward", 5);
-        checkin.put("streak", 2);
-        checkin.put("streaked", 10);
+        checkin.put("balance", balance);
+        checkin.put("reward", checkinReward);
+        checkin.put("streak", streak);
+        checkin.put("streaked", streaked);
         ret.put("checkin", checkin);
     }
 }
