c2edf2f2578a66f1f8c0755b2920917107c4cd1d
Liang Ding <dl88250@gmail.com>
2016-07-20 18:13:07 +0800

    改进 - 移除冗余注释

--- src/main/java/org/b3log/symphony/api/CommentProcessor.java
+++ src/main/java/org/b3log/symphony/api/CommentProcessor.java
@@ -13,11 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.b3log.symphony.api;
 
 import java.io.BufferedReader;
