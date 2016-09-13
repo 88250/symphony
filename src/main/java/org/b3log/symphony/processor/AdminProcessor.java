efb8a99939be466b5b32c66e15ac6f1d47b7b625
Liang Ding <DL88250@gmail.com>
2016-09-13 23:05:15 +0800

    消缺 - 领域管理报错
    @billho 已经存在标签的情况下没有返回标签 id，导致后面领域-标签关联表的 tag_oId 字段为 ""，最终展示领域时的查询会报错

--- src/main/java/org/b3log/symphony/processor/AdminProcessor.java
+++ src/main/java/org/b3log/symphony/processor/AdminProcessor.java
@@ -130,7 +130,7 @@ import org.json.JSONObject;
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author Bill Ho
- * @version 2.21.4.13, Sep 12, 2016
+ * @version 2.21.5.13, Sep 13, 2016
  * @since 1.1.0
  */
 @RequestProcessor
@@ -2167,8 +2167,11 @@ public class AdminProcessor {
             throws Exception {
         String tagTitle = request.getParameter(Tag.TAG_TITLE);
         final JSONObject tag = tagQueryService.getTagByTitle(tagTitle);
-        String tagId = "";
-        if (null == tag) {
+        
+        String tagId;
+        if (tag != null) {
+            tagId = tag.optString(Keys.OBJECT_ID);
+        } else {
             try {
                 if (Strings.isEmptyOrNull(tagTitle)) {
                     throw new Exception(langPropsService.get("tagsErrorLabel"));
