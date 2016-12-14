5997cabf152ba6cf6768856a2727cf501249df13
O型血橙 <zephyrjung@126.com>
2016-12-14 19:43:05 +0800

    :construction: 初步完成概览
    然而别名值得注意
    设置处仍未完成

--- src/test/java/org/b3log/symphony/util/ToCodePoint.java
+++ src/test/java/org/b3log/symphony/util/ToCodePoint.java
@@ -1,9 +1,12 @@
 package org.b3log.symphony.util;
 
+import com.vdurmont.emoji.EmojiParser;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -12,21 +15,35 @@ import java.io.IOException;
  */
 public class ToCodePoint {
     public static void main(String[] args){
-        readFileByLines("D:/index.html");
+        String output = readFileByLines("D:/index.html");
+        FileOutputStream fs = null;
+        try {
+            fs = new FileOutputStream(new File("D:\\index2.html"));
+            byte b[] = output.getBytes();
+            fs.write(b);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public static void readFileByLines(String fileName)  {
+
+    public static String readFileByLines(String fileName)  {
         File file = new File(fileName);
         BufferedReader reader = null;
+        String output = "";
         try {
-            System.out.println("以行为单位读取文件内容，一次读一整行：");
             reader = new BufferedReader(new FileReader(fileName));
             String tempString = null;
             int line = 1;
             // 一次读入一行，直到读入null为文件结束
             while ((tempString = reader.readLine()) != null) {
-                // 显示行号
-//                System.out.println("line " + line + ": " + tempString);
-                System.out.println(StringUtils.substringBetween(tempString,"<img src=\"",".png\">"));
+                String search = StringUtils.substringBetween(tempString,"<img src=\"graphics/emojis/",".png\">");
+                String codePoint = Integer.toHexString(EmojiParser.parseToUnicode(":"+search+":").codePointAt(0));
+//                System.out.println(search + ": " + codePoint);
+                if(null != search)
+                    tempString = StringUtils.replaceOnce(tempString,search,codePoint);
+                output += tempString + "\n";
                 line++;
             }
             reader.close();
@@ -39,6 +56,7 @@ public class ToCodePoint {
                 } catch (IOException e1) {
                 }
             }
+            return output;
         }
     }
 }
