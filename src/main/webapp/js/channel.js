ccf56658f7e5b25bbdc957793bb138ec9e5f6f18
Van <lly219@gmail.com>
2017-02-15 18:42:05 +0800

    :sparkles: 他人回帖不定位，只有自己回帖才定位

--- src/main/webapp/js/channel.js
+++ src/main/webapp/js/channel.js
@@ -80,8 +80,10 @@ var ArticleChannel = {
                     // ua
                     $("#" + data.commentId + ' .cmt-via').text(Util.getDeviceByUa(data.commentUA));
 
-                    // 回帖高亮
-                    Comment._bgFade($("#" + data.commentId));
+                    // 回帖高亮，他人回帖不定位，只有自己回帖才定位
+                    if (Label.currentUserName === data.commentAuthorName) {
+                        Comment._bgFade($("#" + data.commentId));
+                    }
 
                     // 代码高亮
                     hljs.initHighlighting.called = false;
