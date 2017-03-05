9f7ee5075465e3c02fc87b7c84381b4a08361b93
Liang Ding <DL88250@gmail.com>
2017-03-05 13:14:47 +0800

    :sparkles: #416 获取领域列表

--- src/main/java/org/b3log/symphony/api/v2/ArticleAPI2.java
+++ src/main/java/org/b3log/symphony/api/v2/ArticleAPI2.java
@@ -19,11 +19,9 @@ package org.b3log.symphony.api.v2;
 
 import org.apache.commons.lang.StringUtils;
 import org.b3log.latke.Keys;
-import org.b3log.latke.Latkes;
 import org.b3log.latke.logging.Level;
 import org.b3log.latke.logging.Logger;
 import org.b3log.latke.model.Pagination;
-import org.b3log.latke.model.User;
 import org.b3log.latke.servlet.HTTPRequestContext;
 import org.b3log.latke.servlet.HTTPRequestMethod;
 import org.b3log.latke.servlet.annotation.After;
@@ -45,7 +43,6 @@ import org.json.JSONObject;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -73,14 +70,6 @@ public class ArticleAPI2 {
      * Logger.
      */
     private static final Logger LOGGER = Logger.getLogger(ArticleAPI2.class);
-    /**
-     * Pagination window size.
-     */
-    private static final int WINDOW_SIZE = 10;
-    /**
-     * Pagination page size.
-     */
-    private static final int PAGE_SIZE = 20;
     /**
      * Article query service.
      */
@@ -133,7 +122,7 @@ public class ArticleAPI2 {
             data = new JSONObject();
 
             data.put(Tag.TAG, tag);
-            cleanTag(tag);
+            V2s.cleanTag(tag);
 
             String sortModeStr = StringUtils.substringAfter(request.getRequestURI(), "/tag/" + tagURI);
             int sortMode;
@@ -164,15 +153,15 @@ public class ArticleAPI2 {
 
             final int avatarViewMode = (int) request.getAttribute(UserExt.USER_AVATAR_VIEW_MODE);
             final List<JSONObject> articles = articleQueryService.getArticlesByTag(avatarViewMode, sortMode, tag,
-                    page, PAGE_SIZE);
+                    page, V2s.PAGE_SIZE);
             data.put(Article.ARTICLES, articles);
-            cleanArticles(articles);
+            V2s.cleanArticles(articles);
 
             final int tagRefCnt = tag.getInt(Tag.TAG_REFERENCE_CNT);
-            final int pageCount = (int) Math.ceil(tagRefCnt / (double) PAGE_SIZE);
+            final int pageCount = (int) Math.ceil(tagRefCnt / (double) V2s.PAGE_SIZE);
 
             final JSONObject pagination = new JSONObject();
-            final List<Integer> pageNums = Paginator.paginate(page, PAGE_SIZE, pageCount, WINDOW_SIZE);
+            final List<Integer> pageNums = Paginator.paginate(page, V2s.PAGE_SIZE, pageCount, V2s.WINDOW_SIZE);
             pagination.put(Pagination.PAGINATION_PAGE_COUNT, pageCount);
             pagination.put(Pagination.PAGINATION_PAGE_NUMS, pageNums);
 
@@ -228,15 +217,15 @@ public class ArticleAPI2 {
             domain.put(Domain.DOMAIN_T_TAGS, (Object) tags);
 
             data.put(Domain.DOMAIN, domain);
-            cleanDomain(domain);
+            V2s.cleanDomain(domain);
 
             final String domainId = domain.optString(Keys.OBJECT_ID);
             final int avatarViewMode = (int) request.getAttribute(UserExt.USER_AVATAR_VIEW_MODE);
 
-            final JSONObject result = articleQueryService.getDomainArticles(avatarViewMode, domainId, page, PAGE_SIZE);
+            final JSONObject result = articleQueryService.getDomainArticles(avatarViewMode, domainId, page, V2s.PAGE_SIZE);
             final List<JSONObject> articles = (List<JSONObject>) result.opt(Article.ARTICLES);
             data.put(Article.ARTICLES, articles);
-            cleanArticles(articles);
+            V2s.cleanArticles(articles);
 
             data.put(Pagination.PAGINATION, result.optJSONObject(Pagination.PAGINATION));
 
@@ -301,9 +290,9 @@ public class ArticleAPI2 {
                     sortMode = 0;
             }
 
-            data = articleQueryService.getRecentArticles(avatarViewMode, sortMode, page, PAGE_SIZE);
+            data = articleQueryService.getRecentArticles(avatarViewMode, sortMode, page, V2s.PAGE_SIZE);
             final List<JSONObject> articles = (List<JSONObject>) data.opt(Article.ARTICLES);
-            cleanArticles(articles);
+            V2s.cleanArticles(articles);
 
             ret.put(Keys.STATUS_CODE, StatusCodes.SUCC);
         } catch (final Exception e) {
@@ -316,143 +305,5 @@ public class ArticleAPI2 {
         ret.put(Common.DATA, data);
 
         context.renderJSON(ret);
-    }
-
-    private void cleanArticles(final List<JSONObject> articles) {
-        for (final JSONObject article : articles) {
-            cleanArticle(article);
-
-            article.remove(Article.ARTICLE_CONTENT);
-            article.remove(Article.ARTICLE_REWARD_POINT);
-            article.remove(Article.ARTICLE_COMMENTABLE);
-            article.remove(Article.ARTICLE_ANONYMOUS_VIEW);
-            article.remove(Article.ARTICLE_REWARD_CONTENT);
-        }
-    }
-
-    private void cleanArticle(final JSONObject article) {
-        article.put(Article.ARTICLE_CREATE_TIME, ((Date) article.opt(Article.ARTICLE_CREATE_TIME)).getTime());
-        article.put(Article.ARTICLE_UPDATE_TIME, ((Date) article.opt(Article.ARTICLE_UPDATE_TIME)).getTime());
-        article.put(Article.ARTICLE_LATEST_CMT_TIME, ((Date) article.opt(Article.ARTICLE_LATEST_CMT_TIME)).getTime());
-
-        article.remove(Article.ARTICLE_T_LATEST_CMT);
-        article.remove(Article.ARTICLE_LATEST_CMT_TIME);
-        article.remove(Article.ARTICLE_LATEST_CMTER_NAME);
-        article.remove(Article.ARTICLE_SYNC_TO_CLIENT);
-        article.remove(Article.ARTICLE_ANONYMOUS);
-        article.remove(Article.ARTICLE_STATUS);
-        article.remove(Article.ARTICLE_T_PARTICIPANTS);
-        article.remove(Article.REDDIT_SCORE);
-        article.remove(Article.ARTICLE_CLIENT_ARTICLE_ID);
-        article.remove(Article.ARTICLE_CITY);
-        article.remove(Article.ARTICLE_IP);
-        article.remove(Article.ARTICLE_EDITOR_TYPE);
-        article.remove(Article.ARTICLE_RANDOM_DOUBLE);
-        article.remove(Article.ARTICLE_CLIENT_ARTICLE_PERMALINK);
-        article.remove(Article.ARTICLE_T_HEAT);
-
-        final JSONObject author = article.optJSONObject(Article.ARTICLE_T_AUTHOR);
-        cleanUser(author);
-    }
-
-    private void cleanDomain(final JSONObject domain) {
-        final String uri = domain.optString(Domain.DOMAIN_URI);
-        domain.put(Domain.DOMAIN_URI, Latkes.getServePath() + "/domain/" + uri);
-
-        final List<JSONObject> tags = (List<JSONObject>) domain.opt(Domain.DOMAIN_T_TAGS);
-        if (null != tags) {
-            for (final JSONObject tag : tags) {
-                cleanTag(tag);
-            }
-        }
-
-        domain.remove(Domain.DOMAIN_TYPE);
-        domain.remove(Domain.DOMAIN_STATUS);
-        domain.remove(Domain.DOMAIN_SEO_DESC);
-        domain.remove(Domain.DOMAIN_SEO_KEYWORDS);
-        domain.remove(Domain.DOMAIN_SEO_TITLE);
-        domain.remove(Domain.DOMAIN_CSS);
-        domain.remove(Domain.DOMAIN_SORT);
-    }
-
-    private void cleanTag(final JSONObject tag) {
-        final String iconPath = tag.optString(Tag.TAG_ICON_PATH);
-        if (StringUtils.isBlank(iconPath)) {
-            tag.put(Tag.TAG_ICON_PATH, "");
-        } else {
-            tag.put(Tag.TAG_ICON_PATH, Latkes.getStaticServePath() + "/images/tags/" + iconPath);
-        }
-
-        final String uri = tag.optString(Tag.TAG_URI);
-        tag.put(Tag.TAG_URI, Latkes.getServePath() + "/tag/" + uri);
-
-        Tag.fillDescription(tag);
-
-        final List<JSONObject> domains = (List<JSONObject>) tag.opt(Tag.TAG_T_DOMAINS);
-        if (null != domains) {
-            for (final JSONObject domain : domains) {
-                cleanDomain(domain);
-            }
-        }
-
-        tag.remove(Tag.TAG_STATUS);
-        tag.remove(Tag.TAG_RANDOM_DOUBLE);
-        tag.remove(Tag.TAG_CSS);
-        tag.remove(Tag.TAG_SEO_DESC);
-        tag.remove(Tag.TAG_SEO_TITLE);
-        tag.remove(Tag.TAG_SEO_KEYWORDS);
-        tag.remove(Tag.TAG_T_DESCRIPTION_TEXT);
-    }
-
-    private void cleanUser(final JSONObject user) {
-        user.remove(UserExt.USER_QQ);
-        user.remove(UserExt.USER_B3_KEY);
-        user.remove(UserExt.USER_POINT_STATUS);
-        user.remove(UserExt.USER_LATEST_LOGIN_IP);
-        user.remove(UserExt.USER_FOLLOWER_STATUS);
-        user.remove(UserExt.USER_GUIDE_STEP);
-        user.remove(UserExt.USER_ONLINE_STATUS);
-        user.remove(UserExt.USER_CURRENT_CHECKIN_STREAK_START);
-        user.remove(UserExt.USER_COMMENT_STATUS);
-        user.remove(UserExt.USER_UA_STATUS);
-        user.remove(UserExt.USER_LATEST_ARTICLE_TIME);
-        user.remove(UserExt.USER_FORGE_LINK_STATUS);
-        user.remove(UserExt.USER_AVATAR_TYPE);
-        user.remove(UserExt.USER_SUB_MAIL_SEND_TIME);
-        user.remove(UserExt.USER_UPDATE_TIME);
-        user.remove(UserExt.USER_SUB_MAIL_STATUS);
-        user.remove(UserExt.USER_JOIN_POINT_RANK);
-        user.remove(UserExt.USER_B3_CLIENT_ADD_COMMENT_URL);
-        user.remove(UserExt.USER_LATEST_LOGIN_TIME);
-        user.remove(User.USER_PASSWORD);
-        user.remove(UserExt.USER_AVATAR_VIEW_MODE);
-        user.remove(UserExt.USER_LONGEST_CHECKIN_STREAK_END);
-        user.remove(UserExt.USER_WATCHING_ARTICLE_STATUS);
-        user.remove(UserExt.USER_LATEST_CMT_TIME);
-        user.remove(UserExt.USER_FOLLOWING_TAG_STATUS);
-        user.remove(UserExt.USER_TIMELINE_STATUS);
-        user.remove(UserExt.USER_B3_CLIENT_UPDATE_ARTICLE_URL);
-        user.remove(UserExt.USER_JOIN_USED_POINT_RANK);
-        user.remove(UserExt.USER_CURRENT_CHECKIN_STREAK_END);
-        user.remove(UserExt.USER_FOLLOWING_ARTICLE_STATUS);
-        user.remove(UserExt.USER_KEYBOARD_SHORTCUTS_STATUS);
-        user.remove(User.USER_EMAIL);
-        user.remove(UserExt.USER_ARTICLE_STATUS);
-        user.remove(UserExt.USER_B3_CLIENT_ADD_ARTICLE_URL);
-        user.remove(UserExt.USER_GEO_STATUS);
-        user.remove(UserExt.USER_LONGEST_CHECKIN_STREAK_START);
-        user.remove(UserExt.USER_NOTIFY_STATUS);
-        user.remove(UserExt.USER_FOLLOWING_USER_STATUS);
-        user.remove(UserExt.SYNC_TO_CLIENT);
-        user.remove(UserExt.USER_ONLINE_FLAG);
-        user.remove(UserExt.USER_TIMEZONE);
-        user.remove(UserExt.USER_LIST_PAGE_SIZE);
-        user.remove(UserExt.USER_MOBILE_SKIN);
-        user.remove(UserExt.USER_SKIN);
-        user.remove(UserExt.USER_STATUS);
-        user.remove(UserExt.USER_COUNTRY);
-        user.remove(UserExt.USER_PROVINCE);
-        user.remove(UserExt.USER_CITY);
-        user.remove(UserExt.USER_COMMENT_VIEW_MODE);
     }
 }
