0e387c570b4316bf85f055b5f812c171cd8472c4
Liang Ding <DL88250@gmail.com>
2017-03-07 21:26:58 +0800

    :sparkles: #416 获取用户关注帖子列表

--- src/main/java/org/b3log/symphony/api/v2/UserAPI2.java
+++ src/main/java/org/b3log/symphony/api/v2/UserAPI2.java
@@ -39,10 +39,7 @@ import org.b3log.symphony.processor.advice.PermissionGrant;
 import org.b3log.symphony.processor.advice.stopwatch.StopwatchEndAdvice;
 import org.b3log.symphony.processor.advice.stopwatch.StopwatchStartAdvice;
 import org.b3log.symphony.processor.advice.validate.UserRegisterValidation;
-import org.b3log.symphony.service.ArticleQueryService;
-import org.b3log.symphony.service.AvatarQueryService;
-import org.b3log.symphony.service.CommentQueryService;
-import org.b3log.symphony.service.UserQueryService;
+import org.b3log.symphony.service.*;
 import org.b3log.symphony.util.StatusCodes;
 import org.json.JSONObject;
 
@@ -57,6 +54,7 @@ import java.util.List;
  * <li>Gets a user (/api/v2/user/{userName}), GET</li>
  * <li>Gets a user's articles (/api/v2/user/{userName}/articles), GET</li>
  * <li>Gets a user's comments (/api/v2/user/{userName}/comments), GET</li>
+ * <li>Gets a user's watching articles (/api/v2/user/{userName}/watching/articles), GET</li>
  * </ul>
  * </p>
  *
@@ -91,6 +89,84 @@ public class UserAPI2 {
      */
     @Inject
     private CommentQueryService commentQueryService;
+    /**
+     * Follow query service.
+     */
+    @Inject
+    private FollowQueryService followQueryService;
+
+    /**
+     * Gets a user's watching articles.
+     *
+     * @param context  the specified context
+     * @param request  the specified request
+     * @param userName the specified username
+     */
+    @RequestProcessing(value = {"/api/v2/user/{userName}/watching/articles"}, method = HTTPRequestMethod.GET)
+    @Before(adviceClass = {StopwatchStartAdvice.class, AnonymousViewCheck.class})
+    @After(adviceClass = {PermissionGrant.class, StopwatchEndAdvice.class})
+    public void getUserWatchingArticles(final HTTPRequestContext context, final HttpServletRequest request, final String userName) {
+        int page = 1;
+        final String p = request.getParameter("p");
+        if (Strings.isNumeric(p)) {
+            page = Integer.parseInt(p);
+        }
+
+        final JSONObject ret = new JSONObject();
+        context.renderJSONPretty(ret);
+
+        ret.put(Keys.STATUS_CODE, StatusCodes.ERR);
+        ret.put(Keys.MSG, "");
+
+        if (UserRegisterValidation.invalidUserName(userName)) {
+            ret.put(Keys.MSG, "User not found");
+            ret.put(Keys.STATUS_CODE, StatusCodes.NOT_FOUND);
+
+            return;
+        }
+
+        JSONObject data = null;
+        try {
+            final JSONObject user = userQueryService.getUserByName(userName);
+            if (null == user) {
+                ret.put(Keys.MSG, "User not found");
+                ret.put(Keys.STATUS_CODE, StatusCodes.NOT_FOUND);
+
+                return;
+            }
+
+            final String followerId = user.optString(Keys.OBJECT_ID);
+            final int avatarViewMode = (int) request.getAttribute(UserExt.USER_AVATAR_VIEW_MODE);
+
+            final JSONObject followingArticlesResult = followQueryService.getWatchingArticles(avatarViewMode,
+                    followerId, page, V2s.PAGE_SIZE);
+            final List<JSONObject> articles = (List<JSONObject>) followingArticlesResult.opt(Keys.RESULTS);
+            V2s.cleanArticles(articles);
+
+            ret.put(Keys.STATUS_CODE, StatusCodes.SUCC);
+            data = new JSONObject();
+            data.put(Article.ARTICLES, articles);
+
+            final int followingArticleCnt = followingArticlesResult.optInt(Pagination.PAGINATION_RECORD_COUNT);
+            final int pageCount = (int) Math.ceil(followingArticleCnt / (double) V2s.PAGE_SIZE);
+
+            final JSONObject pagination = new JSONObject();
+            final List<Integer> pageNums = Paginator.paginate(page, V2s.PAGE_SIZE, pageCount, V2s.WINDOW_SIZE);
+            pagination.put(Pagination.PAGINATION_PAGE_COUNT, pageCount);
+            pagination.put(Pagination.PAGINATION_PAGE_NUMS, pageNums);
+
+            data.put(Pagination.PAGINATION, pagination);
+
+            ret.put(Keys.STATUS_CODE, StatusCodes.SUCC);
+        } catch (final Exception e) {
+            final String msg = "Gets a user's watching articles failed";
+
+            LOGGER.log(Level.ERROR, msg, e);
+            ret.put(Keys.MSG, msg);
+        }
+
+        ret.put(Common.DATA, data);
+    }
 
     /**
      * Gets a user's comments.
