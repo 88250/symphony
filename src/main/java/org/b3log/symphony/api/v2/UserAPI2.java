140d309f7ce889a43d51422c0605bdc31380ed10
Liang Ding <DL88250@gmail.com>
2017-03-08 12:19:13 +0800

    :sparkles: #416 获取用户收藏帖子列表

--- src/main/java/org/b3log/symphony/api/v2/UserAPI2.java
+++ src/main/java/org/b3log/symphony/api/v2/UserAPI2.java
@@ -93,6 +93,79 @@ public class UserAPI2 {
     private FollowQueryService followQueryService;
 
     /**
+     * Gets a user's following articles.
+     *
+     * @param context  the specified context
+     * @param request  the specified request
+     * @param userName the specified username
+     */
+    @RequestProcessing(value = {"/api/v2/user/{userName}/following/articles"}, method = HTTPRequestMethod.GET)
+    @Before(adviceClass = {StopwatchStartAdvice.class, AnonymousViewCheck.class})
+    @After(adviceClass = {PermissionGrant.class, StopwatchEndAdvice.class})
+    public void getUserFollowingArticles(final HTTPRequestContext context, final HttpServletRequest request, final String userName) {
+        int page = 1;
+        final String p = request.getParameter("p");
+        if (Strings.isNumeric(p)) {
+            page = Integer.parseInt(p);
+        }
+
+        final JSONObject ret = new JSONObject();
+        context.renderJSONPretty(ret);
+
+        ret.put(Keys.STATUS_CODE, StatusCodes.ERR);
+        ret.put(Keys.MSG, "");
+
+        if (UserRegisterValidation.invalidUserName(userName)) {
+            ret.put(Keys.MSG, "User not found");
+            ret.put(Keys.STATUS_CODE, StatusCodes.NOT_FOUND);
+
+            return;
+        }
+
+        JSONObject data = null;
+        try {
+            final JSONObject user = userQueryService.getUserByName(userName);
+            if (null == user) {
+                ret.put(Keys.MSG, "User not found");
+                ret.put(Keys.STATUS_CODE, StatusCodes.NOT_FOUND);
+
+                return;
+            }
+
+            final String followerId = user.optString(Keys.OBJECT_ID);
+            final int avatarViewMode = (int) request.getAttribute(UserExt.USER_AVATAR_VIEW_MODE);
+
+            final JSONObject followingArticlesResult = followQueryService.getFollowingArticles(avatarViewMode,
+                    followerId, page, V2s.PAGE_SIZE);
+            final List<JSONObject> articles = (List<JSONObject>) followingArticlesResult.opt(Keys.RESULTS);
+            V2s.cleanArticles(articles);
+
+            ret.put(Keys.STATUS_CODE, StatusCodes.SUCC);
+            data = new JSONObject();
+            data.put(Article.ARTICLES, articles);
+
+            final int followingArticleCnt = followingArticlesResult.optInt(Pagination.PAGINATION_RECORD_COUNT);
+            final int pageCount = (int) Math.ceil(followingArticleCnt / (double) V2s.PAGE_SIZE);
+
+            final JSONObject pagination = new JSONObject();
+            final List<Integer> pageNums = Paginator.paginate(page, V2s.PAGE_SIZE, pageCount, V2s.WINDOW_SIZE);
+            pagination.put(Pagination.PAGINATION_PAGE_COUNT, pageCount);
+            pagination.put(Pagination.PAGINATION_PAGE_NUMS, pageNums);
+
+            data.put(Pagination.PAGINATION, pagination);
+
+            ret.put(Keys.STATUS_CODE, StatusCodes.SUCC);
+        } catch (final Exception e) {
+            final String msg = "Gets a user's following articles failed";
+
+            LOGGER.log(Level.ERROR, msg, e);
+            ret.put(Keys.MSG, msg);
+        }
+
+        ret.put(Common.DATA, data);
+    }
+
+    /**
      * Gets a user's following tags.
      *
      * @param context  the specified context
@@ -281,16 +354,16 @@ public class UserAPI2 {
             final String followerId = user.optString(Keys.OBJECT_ID);
             final int avatarViewMode = (int) request.getAttribute(UserExt.USER_AVATAR_VIEW_MODE);
 
-            final JSONObject followingArticlesResult = followQueryService.getWatchingArticles(avatarViewMode,
+            final JSONObject watchingArticlesResult = followQueryService.getWatchingArticles(avatarViewMode,
                     followerId, page, V2s.PAGE_SIZE);
-            final List<JSONObject> articles = (List<JSONObject>) followingArticlesResult.opt(Keys.RESULTS);
+            final List<JSONObject> articles = (List<JSONObject>) watchingArticlesResult.opt(Keys.RESULTS);
             V2s.cleanArticles(articles);
 
             ret.put(Keys.STATUS_CODE, StatusCodes.SUCC);
             data = new JSONObject();
             data.put(Article.ARTICLES, articles);
 
-            final int followingArticleCnt = followingArticlesResult.optInt(Pagination.PAGINATION_RECORD_COUNT);
+            final int followingArticleCnt = watchingArticlesResult.optInt(Pagination.PAGINATION_RECORD_COUNT);
             final int pageCount = (int) Math.ceil(followingArticleCnt / (double) V2s.PAGE_SIZE);
 
             final JSONObject pagination = new JSONObject();
