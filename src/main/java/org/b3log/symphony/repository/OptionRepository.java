f2e7c446ca797059ed69c182133789b76464214f
Liang Ding <DL88250@gmail.com>
2017-07-16 11:00:12 +0800

    :art: Clean code

--- src/main/java/org/b3log/symphony/repository/OptionRepository.java
+++ src/main/java/org/b3log/symphony/repository/OptionRepository.java
@@ -30,7 +30,7 @@ import org.json.JSONObject;
  * Option repository.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.1.0, Aug 11, 2016
+ * @version 1.2.1.0, Jul 16, 2017
  * @since 0.2.0
  */
 @Repository
@@ -50,6 +50,13 @@ public class OptionRepository extends AbstractRepository {
     }
 
     @Override
+    public void remove(final String id) throws RepositoryException {
+        super.remove(id);
+
+        optionCache.removeOption(id);
+    }
+
+    @Override
     public JSONObject get(final String id) throws RepositoryException {
         JSONObject ret = optionCache.getOption(id);
         if (null != ret) {
@@ -57,7 +64,6 @@ public class OptionRepository extends AbstractRepository {
         }
 
         ret = super.get(id);
-
         if (null == ret) {
             return null;
         }
