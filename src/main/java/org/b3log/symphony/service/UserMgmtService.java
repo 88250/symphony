6929d76c978f3767ad0f2edff2d24bcba76619ad
Liang Ding <dl88250@gmail.com>
2017-09-05 09:47:17 +0800

    :bug: 添加用户

--- src/main/java/org/b3log/symphony/service/UserMgmtService.java
+++ src/main/java/org/b3log/symphony/service/UserMgmtService.java
@@ -409,7 +409,8 @@ public class UserMgmtService {
             final String userEmail = requestJSONObject.optString(User.USER_EMAIL).trim().toLowerCase();
             final String userName = requestJSONObject.optString(User.USER_NAME);
             JSONObject user = userRepository.getByName(userName);
-            if (null != user || UserExt.NULL_USER_NAME.equals(userName)) {
+            if (null != user && (UserExt.USER_STATUS_C_VALID == user.optInt(UserExt.USER_STATUS)
+                    || UserExt.NULL_USER_NAME.equals(userName))) {
                 if (transaction.isActive()) {
                     transaction.rollback();
                 }
