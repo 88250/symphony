4c3ed9a399ef2eddae553629f5903ac53e7b4ddd
Liang Ding <DL88250@gmail.com>
2017-12-07 19:14:22 +0800

    :sparkles: 站外跳转中间页

---
+++ src/main/java/org/b3log/symphony/processor/ForwardProcessor.java
@@ -0,0 +1,82 @@
+/*
+ * Symphony - A modern community (forum/SNS/blog) platform written in Java.
+ * Copyright (C) 2012-2017,  b3log.org & hacpai.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.b3log.symphony.processor;
+
+import org.apache.commons.lang.StringUtils;
+import org.b3log.latke.ioc.inject.Inject;
+import org.b3log.latke.servlet.HTTPRequestContext;
+import org.b3log.latke.servlet.HTTPRequestMethod;
+import org.b3log.latke.servlet.annotation.After;
+import org.b3log.latke.servlet.annotation.Before;
+import org.b3log.latke.servlet.annotation.RequestProcessing;
+import org.b3log.latke.servlet.annotation.RequestProcessor;
+import org.b3log.latke.servlet.renderer.freemarker.AbstractFreeMarkerRenderer;
+import org.b3log.symphony.model.Common;
+import org.b3log.symphony.processor.advice.PermissionGrant;
+import org.b3log.symphony.processor.advice.stopwatch.StopwatchEndAdvice;
+import org.b3log.symphony.processor.advice.stopwatch.StopwatchStartAdvice;
+import org.b3log.symphony.service.DataModelService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Forward processor.
+ * <ul>
+ * <li>Shows forward page (/forward), GET</li>
+ * </ul>
+ *
+ * @author <a href="http://88250.b3log.org">Liang Ding</a>
+ * @version 1.0.0.0, Dec 7, 2017
+ * @since 2.3.0
+ */
+@RequestProcessor
+public class ForwardProcessor {
+
+    /**
+     * Data model service.
+     */
+    @Inject
+    private DataModelService dataModelService;
+
+    /**
+     * Shows jump page.
+     *
+     * @param response the specified response
+     * @throws Exception exception
+     */
+    @RequestProcessing(value = "/forward", method = HTTPRequestMethod.GET)
+    @Before(adviceClass = {StopwatchStartAdvice.class})
+    @After(adviceClass = {PermissionGrant.class, StopwatchEndAdvice.class})
+    public void showForward(final HTTPRequestContext context, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final AbstractFreeMarkerRenderer renderer = new SkinRenderer(request);
+        context.setRenderer(renderer);
+        renderer.setTemplateName("forward.ftl");
+        final Map<String, Object> dataModel = renderer.getDataModel();
+
+        String to = request.getParameter(Common.GOTO);
+        if (StringUtils.isBlank(to)) {
+            to = "https://hacpai.com";
+        }
+        dataModel.put("forwardURL", to);
+
+        dataModelService.fillHeaderAndFooter(request, response, dataModel);
+    }
+
+}
