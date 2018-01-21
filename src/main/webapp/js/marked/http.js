22882e908a3f8edadd36e17295e3bbedfab6f3bc
Liang Ding <dl88250@gmail.com>
2018-01-21 11:55:25 +0800

    :loud_sound: marked 加入系统信号处理日志

--- src/main/webapp/js/marked/http.js
+++ src/main/webapp/js/marked/http.js
@@ -21,7 +21,7 @@
  *
  * @author <a href="http://vanessa.b3log.org">Liyuan Li</a>
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.0.0, May 20, 2017
+ * @version 1.2.0.0, Jan 21, 2018
  * @since 1.7.0
  */
 
@@ -32,41 +32,65 @@ var marked = require('marked');
 var renderer = new marked.Renderer();
 
 renderer.listitem = function (text, level) {
-    if (text.indexOf('[ ] ') === 0 && text.replace(/\s/g, '') !== '[]') {
-        text = '<input type="checkbox" disabled>' + text.replace('[ ]', '');
-        return `<li class="task-item">${text}</li>`;
-    } else if (text.indexOf('[x] ') === 0 && text.replace(/\s/g, '') !== '[x]') {
-        text = '<input type="checkbox" checked disabled>' + text.replace('[x]', '');
-        return `<li class="task-item">${text}</li>`;
-    }
+  if (text.indexOf('[ ] ') === 0 && text.replace(/\s/g, '') !== '[]') {
+    text = '<input type="checkbox" disabled>' + text.replace('[ ]', '');
+    return `<li class="task-item">${text}</li>`;
+  } else if (text.indexOf('[x] ') === 0 && text.replace(/\s/g, '') !== '[x]') {
+    text = '<input type="checkbox" checked disabled>' + text.replace('[x]', '');
+    return `<li class="task-item">${text}</li>`;
+  }
 
-    return `<li>${text}</li>`;
+  return `<li>${text}</li>`;
 };
 
 marked.setOptions({
-    renderer: renderer,
-    gfm: true,
-    tables: true,
-    breaks: true,
-    smartLists: true
+  renderer: renderer,
+  gfm: true,
+  tables: true,
+  breaks: true,
+  smartLists: true
 });
 
 process.on('uncaughtException', function (err) {
-    console.log(err);
+  console.log(err);
+});
+
+process.on('exit', function () {
+  console.log("exit");
+});
+
+process.on('SIGTERM', function () {
+  console.log("on signal [SIGTERM]");
+  process.exit(0);
+});
+
+process.on('SIGINT', function () {
+  console.log("on signal [SIGINT]");
+  process.exit(0);
+});
+
+process.on('SIGUSR1', function () {
+  console.log("on signal [SIGUSR1]");
+  process.exit(0);
+});
+
+process.on('SIGUSR2', function () {
+  console.log("on signal [SIGUSR2]");
+  process.exit(0);
 });
 
 var server = http.createServer(function (request, response) {
-    var mdContent = '';
+  var mdContent = '';
 
-    request.on('data', function (data) {
-        mdContent += data;
-    });
+  request.on('data', function (data) {
+    mdContent += data;
+  });
 
-    request.on('end', function () {
-        response.write(marked(mdContent));
+  request.on('end', function () {
+    response.write(marked(mdContent));
 
-        response.end();
-    });
+    response.end();
+  });
 });
 
 server.listen(PORT);
