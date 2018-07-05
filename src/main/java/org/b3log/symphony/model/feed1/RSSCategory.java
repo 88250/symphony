fdc41f2cb1ef990925771723605232504c8bf31c
Liang Ding <d@b3log.org>
2018-07-05 17:17:09 +0800

    :truck: 重命名

--- src/main/java/org/b3log/symphony/model/feed1/RSSCategory.java
+++ src/main/java/org/b3log/symphony/model/feed1/RSSCategory.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package org.b3log.symphony.model.Feed;
+package org.b3log.symphony.model.feed;
 
 import org.apache.commons.lang.StringEscapeUtils;
 
