3d0507c8d1a42d35261fc688023e96ba7511deb5
Liang Ding <d@b3log.org>
2018-08-02 09:23:14 +0800

    :art: 工具类

---
+++ src/main/java/org/b3log/symphony/util/Networks.java
@@ -0,0 +1,57 @@
+/*
+ * Symphony - A modern community (forum/BBS/SNS/blog) platform written in Java.
+ * Copyright (C) 2012-2018, b3log.org & hacpai.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.b3log.symphony.util;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Network utilities.
+ *
+ * @author <a href="http://88250.b3log.org">Liang Ding</a>
+ * @version 1.0.2.1, Aug 2, 2018
+ * @since 1.3.0
+ */
+public final class Networks {
+
+    /**
+     * Is IPv4.
+     *
+     * @param ip ip
+     * @return {@code true} if it is, returns {@code false} otherwise
+     */
+    public static boolean isIPv4(final String ip) {
+        if (StringUtils.isBlank(ip)) {
+            return false;
+        }
+
+        final String regex = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(ip);
+
+        return matcher.matches();
+    }
+
+    /**
+     * Private constructor.
+     */
+    private Networks() {
+    }
+}
