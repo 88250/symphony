0e3e0509139120854160cc32293cc08d861eacdb
Liang Ding <d@b3log.org>
2018-08-29 09:21:18 +0800

    :bookmark: 发布 v3.3.0

--- src/main/java/org/b3log/symphony/SymphonyServletListener.java
+++ src/main/java/org/b3log/symphony/SymphonyServletListener.java
@@ -59,7 +59,7 @@ import java.util.Locale;
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author Bill Ho
- * @version 3.19.10.12, Aug 2, 2018
+ * @version 3.19.10.13, Aug 29, 2018
  * @since 0.2.0
  */
 public final class SymphonyServletListener extends AbstractServletListener {
@@ -72,7 +72,7 @@ public final class SymphonyServletListener extends AbstractServletListener {
     /**
      * Symphony version.
      */
-    public static final String VERSION = "3.2.0";
+    public static final String VERSION = "3.3.0";
 
     /**
      * JSONO print indent factor.
