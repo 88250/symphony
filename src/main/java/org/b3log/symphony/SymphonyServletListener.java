6a8c5504f23486de11b069c209c59613bee5ba72
Liang Ding <d@b3log.org>
2018-09-24 15:41:45 +0800

    :pencil2: Fix typos

--- src/main/java/org/b3log/symphony/SymphonyServletListener.java
+++ src/main/java/org/b3log/symphony/SymphonyServletListener.java
@@ -74,7 +74,7 @@ public final class SymphonyServletListener extends AbstractServletListener {
     public static final String VERSION = "3.4.0";
 
     /**
-     * JSONO print indent factor.
+     * JSON print indent factor.
      */
     public static final int JSON_PRINT_INDENT_FACTOR = 4;
 
