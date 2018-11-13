2751e3cdb03c4ef9a20dc23c14df2b280fd3b30c
Liang Ding <d@b3log.org>
2018-11-13 12:15:42 +0800

    :art: Cleanup code

--- src/main/java/org/b3log/symphony/service/NotificationQueryService.java
+++ src/main/java/org/b3log/symphony/service/NotificationQueryService.java
@@ -44,7 +44,7 @@ import java.util.List;
  * Notification query service.
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.14.0.2, Jul 15, 2018
+ * @version 1.14.0.3, Nov 13, 2018
  * @since 0.2.5
  */
 @Service
@@ -1309,17 +1309,14 @@ public class NotificationQueryService {
      * </pre>
      * @throws ServiceException service exception
      */
-    public JSONObject getBroadcastNotifications(final int avatarViewMode,
-                                                final String userId, final int currentPageNum, final int pageSize) throws ServiceException {
+    public JSONObject getBroadcastNotifications(final int avatarViewMode, final String userId, final int currentPageNum, final int pageSize) throws ServiceException {
         final JSONObject ret = new JSONObject();
         final List<JSONObject> rslts = new ArrayList<>();
-
         ret.put(Keys.RESULTS, (Object) rslts);
 
         final List<Filter> filters = new ArrayList<>();
         filters.add(new PropertyFilter(Notification.NOTIFICATION_USER_ID, FilterOperator.EQUAL, userId));
         filters.add(new PropertyFilter(Notification.NOTIFICATION_DATA_TYPE, FilterOperator.EQUAL, Notification.DATA_TYPE_C_BROADCAST));
-
         final Query query = new Query().setCurrentPageNum(currentPageNum).setPageSize(pageSize).
                 setFilter(new CompositeFilter(CompositeFilterOperator.AND, filters)).
                 addSort(Notification.NOTIFICATION_HAS_READ, SortDirection.ASCENDING).
@@ -1328,14 +1325,12 @@ public class NotificationQueryService {
         try {
             final JSONObject queryResult = notificationRepository.get(query);
             final JSONArray results = queryResult.optJSONArray(Keys.RESULTS);
-
             ret.put(Pagination.PAGINATION_RECORD_COUNT,
                     queryResult.optJSONObject(Pagination.PAGINATION).optInt(Pagination.PAGINATION_RECORD_COUNT));
 
             for (int i = 0; i < results.length(); i++) {
                 final JSONObject notification = results.optJSONObject(i);
                 final String articleId = notification.optString(Notification.NOTIFICATION_DATA_ID);
-
                 final Query q = new Query().setPageCount(1).
                         addProjection(Article.ARTICLE_TITLE, String.class).
                         addProjection(Article.ARTICLE_TYPE, Integer.class).
@@ -1350,17 +1345,15 @@ public class NotificationQueryService {
                 final JSONObject article = rlts.optJSONObject(0);
 
                 if (null == article) {
-                    LOGGER.warn("Not found article[id=" + articleId + ']');
-
+                    LOGGER.warn("Not found article [id=" + articleId + ']');
                     continue;
                 }
 
                 final String articleTitle = article.optString(Article.ARTICLE_TITLE);
                 final String articleAuthorId = article.optString(Article.ARTICLE_AUTHOR_ID);
                 final JSONObject author = userRepository.get(articleAuthorId);
-
                 if (null == author) {
-                    LOGGER.warn("Not found user[id=" + articleAuthorId + ']');
+                    LOGGER.warn("Not found user [id=" + articleAuthorId + ']');
 
                     continue;
                 }
@@ -1369,21 +1362,17 @@ public class NotificationQueryService {
                 broadcastNotification.put(Keys.OBJECT_ID, notification.optString(Keys.OBJECT_ID));
                 broadcastNotification.put(Common.AUTHOR_NAME, author.optString(User.USER_NAME));
                 broadcastNotification.put(Common.CONTENT, "");
-                broadcastNotification.put(Common.THUMBNAIL_URL,
-                        avatarQueryService.getAvatarURLByUser(avatarViewMode, author, "48"));
+                broadcastNotification.put(Common.THUMBNAIL_URL, avatarQueryService.getAvatarURLByUser(avatarViewMode, author, "48"));
                 broadcastNotification.put(Article.ARTICLE_TITLE, Emotions.convert(articleTitle));
                 broadcastNotification.put(Common.URL, Latkes.getServePath() + article.optString(Article.ARTICLE_PERMALINK));
                 broadcastNotification.put(Common.CREATE_TIME, new Date(article.optLong(Article.ARTICLE_CREATE_TIME)));
-                broadcastNotification.put(Notification.NOTIFICATION_HAS_READ,
-                        notification.optBoolean(Notification.NOTIFICATION_HAS_READ));
+                broadcastNotification.put(Notification.NOTIFICATION_HAS_READ, notification.optBoolean(Notification.NOTIFICATION_HAS_READ));
                 broadcastNotification.put(Common.TYPE, Article.ARTICLE);
                 broadcastNotification.put(Article.ARTICLE_TYPE, article.optInt(Article.ARTICLE_TYPE));
-
                 final String tagsStr = article.optString(Article.ARTICLE_TAGS);
                 broadcastNotification.put(Article.ARTICLE_TAGS, tagsStr);
                 final List<JSONObject> tags = tagQueryService.buildTagObjs(tagsStr);
                 broadcastNotification.put(Article.ARTICLE_T_TAG_OBJS, (Object) tags);
-
                 broadcastNotification.put(Article.ARTICLE_COMMENT_CNT, article.optInt(Article.ARTICLE_COMMENT_CNT));
                 broadcastNotification.put(Article.ARTICLE_PERFECT, article.optInt(Article.ARTICLE_PERFECT));
 
@@ -1392,7 +1381,7 @@ public class NotificationQueryService {
 
             return ret;
         } catch (final RepositoryException e) {
-            LOGGER.log(Level.ERROR, "Gets [broadcast] notifications", e);
+            LOGGER.log(Level.ERROR, "Get [broadcast] notifications", e);
 
             throw new ServiceException(e);
         }
