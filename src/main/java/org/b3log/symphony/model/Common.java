865a3e86640028fcc0e2b8eeabae2290336fb675
Liang Ding <d@b3log.org>
2019-01-02 21:27:39 +0800

    :tada: :page_facing_up: 2019
    2019 新年快乐！

--- src/main/java/org/b3log/symphony/model/Common.java
+++ src/main/java/org/b3log/symphony/model/Common.java
@@ -1,6 +1,6 @@
 /*
  * Symphony - A modern community (forum/BBS/SNS/blog) platform written in Java.
- * Copyright (C) 2012-2018, b3log.org & hacpai.com
+ * Copyright (C) 2012-2019, b3log.org & hacpai.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
