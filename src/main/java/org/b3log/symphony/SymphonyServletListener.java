f537c76459989ba4ccbd80dd2fff54f54589c623
Liang Ding <d@b3log.org>
2019-01-10 09:23:03 +0800

    :bookmark: 发布 v3.4.6

--- src/main/java/org/b3log/symphony/SymphonyServletListener.java
+++ src/main/java/org/b3log/symphony/SymphonyServletListener.java
@@ -56,7 +56,7 @@ import java.util.Locale;
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author Bill Ho
- * @version 3.19.10.26, Dec 31, 2018
+ * @version 3.19.10.27, Jan 10, 2019
  * @since 0.2.0
  */
 public final class SymphonyServletListener extends AbstractServletListener {
@@ -69,7 +69,7 @@ public final class SymphonyServletListener extends AbstractServletListener {
     /**
      * Symphony version.
      */
-    public static final String VERSION = "3.4.5";
+    public static final String VERSION = "3.4.6";
 
     /**
      * Bean manager.
