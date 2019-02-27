01c6b6faecb3326dd9d320359c91c94ec4661b31
Liang Ding <d@b3log.org>
2019-02-27 11:17:18 +0800

    :recycle: Fix #882

--- src/main/java/org/b3log/symphony/util/Mails.java
+++ src/main/java/org/b3log/symphony/util/Mails.java
@@ -31,20 +31,14 @@ import org.b3log.latke.util.URLs;
 import org.b3log.symphony.model.Common;
 import org.json.JSONObject;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileTypeMap;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.File;
+import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -56,7 +50,7 @@ import java.util.regex.Pattern;
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author <a href="http://blog.thinkjava.top">VirutalPier</a>
  * @author <a href="https://github.com/snowflake3721">snowflake</a>
- * @version 1.2.6.8, Aug 30, 2018
+ * @version 1.2.6.8, Feb 27, 2019
  * @since 1.3.0
  */
 public final class Mails {
@@ -237,10 +231,11 @@ public final class Mails {
                     formData.put("to", toMail);
                     formData.put("html", html);
 
-                    final HttpResponse response = HttpRequest.post("http://api.sendcloud.net/apiv2/mail/send").form(formData).timeout(5000).send();
+                    final HttpResponse response = HttpRequest.post("http://api.sendcloud.net/apiv2/mail/send").form(formData).header(Common.USER_AGENT, Symphonys.USER_AGENT_BOT).
+                            connectionTimeout(5000).timeout(5000).send();
+                    response.close();
                     response.charset("UTF-8");
                     LOGGER.debug(response.bodyText());
-                    response.close();
 
                     return;
                 default:
@@ -331,9 +326,11 @@ public final class Mails {
                             xsmtpapi.put("sub", new JSONObject());
                             formData.put("xsmtpapi", xsmtpapi.toString());
 
-                            response = HttpRequest.post("http://api.sendcloud.net/apiv2/mail/sendtemplate").form(formData).send();
-                            LOGGER.debug(response.bodyText());
+                            response = HttpRequest.post("http://api.sendcloud.net/apiv2/mail/sendtemplate").form(formData).header(Common.USER_AGENT, Symphonys.USER_AGENT_BOT).
+                                    connectionTimeout(5000).timeout(30000).send();
                             response.close();
+                            response.charset("UTF-8");
+                            LOGGER.debug(response.bodyText());
 
                             LOGGER.info("Sent [" + batch.size() + "] mails");
                         } catch (final Exception e) {
@@ -359,9 +356,11 @@ public final class Mails {
                         xsmtpapi.put("sub", new JSONObject());
                         formData.put("xsmtpapi", xsmtpapi.toString());
 
-                        response = HttpRequest.post("http://api.sendcloud.net/apiv2/mail/sendtemplate").form(formData).send();
-                        LOGGER.debug(response.bodyText());
+                        response = HttpRequest.post("http://api.sendcloud.net/apiv2/mail/sendtemplate").form(formData).header(Common.USER_AGENT, Symphonys.USER_AGENT_BOT).
+                                connectionTimeout(5000).timeout(30000).send();
                         response.close();
+                        response.charset("UTF-8");
+                        LOGGER.debug(response.bodyText());
 
                         LOGGER.info("Sent [" + batch.size() + "] mails");
                     } catch (final Exception e) {
@@ -418,10 +417,11 @@ public final class Mails {
 
         map.put("Signature", Crypts.signHmacSHA1(stringToSign.toString(), accessSecret + "&"));
 
-        final HttpResponse response = HttpRequest.post("https://dm.aliyuncs.com").form(map).send();
-        LOGGER.debug(response.bodyText());
-
+        final HttpResponse response = HttpRequest.post("https://dm.aliyuncs.com").form(map).header(Common.USER_AGENT, Symphonys.USER_AGENT_BOT).
+                connectionTimeout(5000).timeout(30000).send();
         response.close();
+        response.charset("UTF-8");
+        LOGGER.debug(response.bodyText());
     }
 
     private static String percentEncode(final String value) {
@@ -456,7 +456,8 @@ public final class Mails {
         addData.put("templateType", "1"); // 批量邮件
 
         addData.put("html", html);
-        HttpRequest.post("http://api.sendcloud.net/apiv2/template/add").form(addData).send();
+        HttpRequest.post("http://api.sendcloud.net/apiv2/template/add").form(addData).header(Common.USER_AGENT, Symphonys.USER_AGENT_BOT).
+                connectionTimeout(5000).timeout(30000).send().close();
 
         final Map<String, Object> updateData = new HashMap<>();
         updateData.put("apiUser", SENDCLOUD_BATCH_API_USER);
@@ -464,7 +465,8 @@ public final class Mails {
         updateData.put("invokeName", TEMPLATE_NAME_WEEKLY);
 
         updateData.put("html", html);
-        HttpRequest.post("http://api.sendcloud.net/apiv2/template/update").form(updateData).send();
+        HttpRequest.post("http://api.sendcloud.net/apiv2/template/update").form(updateData).header(Common.USER_AGENT, Symphonys.USER_AGENT_BOT).
+                connectionTimeout(5000).timeout(30000).send().close();
     }
 }
 
@@ -487,7 +489,7 @@ final class MailSender implements java.io.Serializable {
 
     public static final String sender = Symphonys.get("mail.local.smtp.sender");
     public static final String username = Symphonys.get("mail.local.smtp.username");
-    public static final String password = Symphonys.get("mail.local.smtp.passsword"); /* 换成自己的密码哦 */
+    public static final String password = Symphonys.get("mail.local.smtp.passsword");
 
     private static final long serialVersionUID = -1000794424345267933L;
     private static final String CHARSET = "text/html;charset=UTF-8";
@@ -499,7 +501,6 @@ final class MailSender implements java.io.Serializable {
     private static final String mail_smtp_auth = Symphonys.get("mail.local.smtp.auth");
     private static final String mail_smtp_ssl = Symphonys.get("mail.local.smtp.ssl");
     private static final String mail_smtp_starttls_enable = Symphonys.get("mail.local.smtp.starttls.enable");
-    private static final String saved_path = Symphonys.get("mail.local.saved.eml.path");
     private static MailSender mailSender;
     private static Properties prop = new Properties();
 
@@ -582,9 +583,7 @@ final class MailSender implements java.io.Serializable {
         return MailSenderHolder.INSTANCE;
     }
 
-    public void sendMessage(String[] tos, String subject, String content, String savedEmlPath, MailType mailType)
-            throws Exception {
-
+    public void sendMessage(String[] tos, String subject, String content) throws Exception {
         // 1、创建session
         Session session = Session.getInstance(prop);
         // 开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
@@ -593,16 +592,9 @@ final class MailSender implements java.io.Serializable {
         Transport ts = session.getTransport();
         // 3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
         ts.connect(mail_host, Integer.valueOf(mail_port), username, password);
-        MimeMessage message = new MimeMessage(session);
         // 4、创建邮件
-        // Message message = createSimpleMail(session);
-
-        if (MailType.SIMPLE.equals(mailType)) {
-            createTextMail(message, sender, tos, subject, content, savedEmlPath);
-        } else if (MailType.IMAGE.equals(mailType)) {
-            message = createImageMail(message, sender, tos, subject, content, savedEmlPath);
-        }
-
+        MimeMessage message = new MimeMessage(session);
+        createTextMail(message, sender, tos, subject, content);
         // 5、发送邮件
         ts.sendMessage(message, message.getAllRecipients());
         LOGGER.debug(Arrays.toString(tos));
@@ -619,30 +611,10 @@ final class MailSender implements java.io.Serializable {
      * @param to
      * @param subject
      * @param content
-     * @param savedEmlPathName
      * @return
      * @throws Exception
      */
-    public MimeMessage createTextMail(MimeMessage message, String from, String[] to, String subject, String content,
-                                      String savedEmlPathName) throws Exception {
-
-        long currentTime = System.currentTimeMillis();
-        if (!savedEmlPathName.endsWith("/")) {
-            savedEmlPathName = savedEmlPathName + "/";
-        }
-
-        // 构造日期目录
-        String datePath = buildDatePath();
-
-        String savedEmlPathNameDate = savedEmlPathName + datePath;
-        File savedEmlPathNameDateFile = new File(savedEmlPathNameDate);
-        if (!savedEmlPathNameDateFile.exists()) {
-            savedEmlPathNameDateFile.mkdirs();
-        }
-
-        // 构造存储文件绝对路径
-        String fileName = savedEmlPathNameDate + currentTime + ".eml";
-
+    public MimeMessage createTextMail(MimeMessage message, String from, String[] to, String subject, String content) throws Exception {
         // 指明邮件的发件人
         message.setFrom(new InternetAddress(from));
         setTo(to, message);
@@ -655,127 +627,9 @@ final class MailSender implements java.io.Serializable {
         message.setSubject(StringUtils.trimToEmpty(subject));
         // 邮件的文本内容
         message.setContent(content, CHARSET);
-
-        saveMessageFile(message, fileName);
         // 返回创建好的邮件对象
-        return message;
-    }
-
-    /**
-     * 生成一封邮件正文带图片的邮件.
-     *
-     * @param message
-     * @param from
-     * @param to
-     * @param subject
-     * @param content
-     * @param savedEmlPath
-     * @return
-     * @throws Exception
-     */
-    public MimeMessage createImageMail(MimeMessage message, String from, String[] to, String subject, String content,
-                                       String savedEmlPath) throws Exception {
-        // 创建邮件
-        // MimeMessage message = new MimeMessage(session);
-        // 设置邮件的基本信息
-        // 发件人
-        message.setFrom(new InternetAddress(from));
-        setTo(to, message);
-        // 邮件标题
-        if (StringUtils.isEmpty(subject)) {
-            subject = "Email from the system [Dromala special community for study]";
-        }
-        message.setSubject(StringUtils.trimToEmpty(subject));
-
-        // 准备邮件数据
-        message = resolveContentForImage(message, content, savedEmlPath);
-
-        // 返回创建好的邮件
-        return message;
-    }
-
-    public MimeMessage resolveContentForImage(MimeMessage message, String content, String savedEmlPathName)
-            throws Exception {
-        long currentTime = System.currentTimeMillis();
-        if (!savedEmlPathName.endsWith("/")) {
-            savedEmlPathName = savedEmlPathName + "/";
-        }
-
-        // 构造日期目录
-        String datePath = buildDatePath();
-
-        String savedEmlPathNameDate = savedEmlPathName + datePath;
-        File savedEmlPathNameDateFile = new File(savedEmlPathNameDate);
-        if (!savedEmlPathNameDateFile.exists()) {
-            savedEmlPathNameDateFile.mkdirs();
-        }
-
-        // 构造存储文件绝对路径
-        String fileName = savedEmlPathNameDate + currentTime + ".eml";
-
-        Set<String> imageStrSet = getImgStr(content);
-        // 准备邮件正文数据
-        MimeBodyPart text = new MimeBodyPart();
-
-        if (null != imageStrSet && imageStrSet.size() > 0) {
-            int i = 0;
-            for (String imageStr : imageStrSet) {
-
-                // 准备图片数据
-                MimeBodyPart image = new MimeBodyPart();
-                String imagenew = currentTime + "/" + i + imageStr.substring(imageStr.lastIndexOf("."));
-                // imagenew = imagenew.replaceAll("/", "_");
-                String savedPathAndFileName = savedEmlPathNameDate + imagenew;
-
-                DataHandler dh = new DataHandler(new UrlDataSource(imageStr, savedPathAndFileName, true));
-                image.setDataHandler(dh);
-                String imagenewcid = "cid:" + imagenew;
-                image.setContentID(imagenew);
-
-                // 描述数据关系
-                MimeMultipart mm = new MimeMultipart();
-                mm.addBodyPart(text);
-                mm.addBodyPart(image);
-                mm.setSubType("related");
-
-                message.setContent(mm);
-                content = content.replaceAll(imageStr, imagenewcid);
-                text.setContent(content, CHARSET);
-                message.saveChanges();
-                i++;
-            }
-
-        }
-        text.setContent(content, CHARSET);
-        saveMessageFile(message, fileName);
 
         return message;
-    }
-
-    private void saveMessageFile(MimeMessage message, String fileName)
-            throws MessagingException, IOException {
-        message.saveChanges();
-        // 将创建好的邮件写入到E盘以文件的形式进行保存
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(fileName);
-            message.writeTo(fos);
-            fos.flush();
-        } finally {
-            if (fos != null) {
-                fos.close();
-            }
-        }
-    }
-
-    private String buildDatePath() {
-        StringBuilder sb = new StringBuilder();
-        Calendar ca = Calendar.getInstance();
-        int year = ca.get(Calendar.YEAR);// 获取年份
-        int month = ca.get(Calendar.MONTH);// 获取月份
-        int day = ca.get(Calendar.DATE);// 获取日
-        sb.append(year).append("/").append(month).append("/").append(day).append("/");
-        return sb.toString();
     }
 
     /**
@@ -806,7 +660,7 @@ final class MailSender implements java.io.Serializable {
 
     private void sendHTML(final String fromName, final String subject, final String[] toMail, final String html) {
         try {
-            getInstance().sendMessage(toMail, subject, html, saved_path, MailType.IMAGE);
+            getInstance().sendMessage(toMail, subject, html);
             LOGGER.debug(html);
 
         } catch (Exception e) {
@@ -814,143 +668,7 @@ final class MailSender implements java.io.Serializable {
         }
     }
 
-    private enum MailType {
-        SIMPLE, IMAGE /* html with image */
-    }
-
     private static class MailSenderHolder {
         private static final MailSender INSTANCE = new MailSender();
-    }
-}
-
-/**
- * 加载网络资源图片保存到本地
- *
- * @author snowflake
- * @version 1.0.0.0, Mar 9, 2016
- * @since 2.0.0
- */
-class UrlDataSource implements DataSource {
-
-    private String urlPath;
-    private String savedFileName;
-    private boolean saved;
-    private FileTypeMap typeMap = null;
-    private File _file;
-
-    public UrlDataSource(String urlPath, String savedFileName, boolean saved) {
-        this.urlPath = urlPath;
-        this.savedFileName = savedFileName;
-        this.saved = saved;
-        File imageFile = new File(this.savedFileName);
-        this._file = imageFile;
-    }
-
-    public static byte[] readInputStream(InputStream inStream) throws IOException {
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        // 创建一个Buffer字符串
-        byte[] buffer = new byte[1024];
-        // 每次读取的字符串长度，如果为-1，代表全部读取完毕
-        int len = 0;
-        // 使用一个输入流从buffer里把数据读取出来
-        while ((len = inStream.read(buffer)) != -1) {
-            // 用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
-            outStream.write(buffer, 0, len);
-        }
-        // 关闭输入流
-        inStream.close();
-        // 把outStream里的数据写入内存
-        return outStream.toByteArray();
-    }
-
-    @Override
-    public InputStream getInputStream() throws IOException {
-
-        return getInputStreamFromURL(this.urlPath, this.saved);
-    }
-
-    @Override
-    public OutputStream getOutputStream() throws IOException {
-
-        if (this._file.isDirectory()) {
-            if (!this._file.exists()) {
-                this._file.mkdirs();
-            }
-        }
-
-        String parentPath = this._file.getParent();
-        File _filePath = new File(parentPath);
-        _filePath.mkdirs();
-
-        return new FileOutputStream(this._file);
-    }
-
-    @Override
-    public String getContentType() {
-
-        // check to see if the type map is null?
-        if (typeMap == null)
-            return FileTypeMap.getDefaultFileTypeMap().getContentType(_file);
-        else
-            return typeMap.getContentType(_file);
-    }
-
-    @Override
-    public String getName() {
-        return _file.getName();
-    }
-
-    private InputStream getInputStreamFromURL(String urlPath, boolean saved) throws IOException {
-        // new一个URL对象
-        URL url = new URL(urlPath);
-        // 打开链接
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        // 设置请求方式为"GET"
-        conn.setRequestMethod("GET");
-        // 超时响应时间为5秒
-        conn.setConnectTimeout(5 * 1000);
-        // 通过输入流获取图片数据
-        InputStream inStream = conn.getInputStream();
-        // 得到图片的二进制数据，以二进制封装得到数据，具有通用性
-        if (saved) {
-            try {
-                saveFile(inStream);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        InputStream fis = new FileInputStream(_file);
-        return fis;
-    }
-
-    // 得到图片的二进制数据，以二进制封装得到数据，具有通用性
-    public void saveFile(InputStream inStream) throws IOException {
-        byte[] data = readInputStream(inStream);
-        // new一个文件对象用来保存图片，默认保存当前工程根目录
-        // File imageFile = new File(fileName);
-        // 创建输出流
-        FileOutputStream outStream = (FileOutputStream) getOutputStream();
-        // 写入数据
-        outStream.write(data);
-        // 关闭输出流
-        outStream.close();
-    }
-
-    /**
-     * Return the File object that corresponds to this FileDataSource.
-     *
-     * @return the File object for the file represented by this object.
-     */
-    public File getFile() {
-        return _file;
-    }
-
-    /**
-     * Set the FileTypeMap to use with this FileDataSource
-     *
-     * @param map The FileTypeMap for this object.
-     */
-    public void setFileTypeMap(FileTypeMap map) {
-        typeMap = map;
     }
 }
