ea435e1066796ac9541d44889949473acfbb9623
Liang Ding <d@b3log.org>
2019-03-08 19:32:58 +0800

    :page_facing_up: 更新文件头

--- src/main/java/org/b3log/symphony/util/Templates.java
+++ src/main/java/org/b3log/symphony/util/Templates.java
@@ -2,7 +2,18 @@
  * Symphony - A modern community (forum/BBS/SNS/blog) platform written in Java.
  * Copyright (C) 2012-2019, b3log.org & hacpai.com
  *
- * 本文件属于 Sym 商业版的一部分，请仔细阅读项目根文件夹的 LICENSE 并严格遵守相关约定
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package org.b3log.symphony.util;
 
