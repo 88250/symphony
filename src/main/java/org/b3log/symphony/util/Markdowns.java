6b30895d10b49a140ba5500d2401bb9b9cb14276
Liang Ding <d@b3log.org>
2019-07-29 11:19:53 +0800

    :arrow_up: 升级 Latke 和 flexmark

--- src/main/java/org/b3log/symphony/util/Markdowns.java
+++ src/main/java/org/b3log/symphony/util/Markdowns.java
@@ -17,10 +17,13 @@
  */
 package org.b3log.symphony.util;
 
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
-import com.vladsch.flexmark.profiles.pegdown.Extensions;
-import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
-import com.vladsch.flexmark.util.options.DataHolder;
+import com.vladsch.flexmark.util.data.DataHolder;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
@@ -48,10 +51,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.*;
 
 /**
@@ -64,7 +64,7 @@ import java.util.concurrent.*;
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
  * @author <a href="http://zephyr.b3log.org">Zephyr</a>
  * @author <a href="http://vanessa.b3log.org">Vanessa</a>
- * @version 1.11.21.14, May 9, 2019
+ * @version 1.11.21.15, Jul 29, 2019
  * @since 0.2.0
  */
 public final class Markdowns {
@@ -87,7 +87,13 @@ public final class Markdowns {
     /**
      * Built-in MD engine options.
      */
-    private static final DataHolder OPTIONS = PegdownOptionsAdapter.flexmarkOptions(Extensions.ALL_WITH_OPTIONALS);
+    private static final DataHolder OPTIONS = new MutableDataSet().
+            set(com.vladsch.flexmark.parser.Parser.EXTENSIONS, Arrays.asList(
+                    TablesExtension.create(),
+                    TaskListExtension.create(),
+                    StrikethroughExtension.create(),
+                    AutolinkExtension.create())).
+            set(HtmlRenderer.SOFT_BREAK, "<br />\n");
 
     /**
      * Built-in MD engine parser.
