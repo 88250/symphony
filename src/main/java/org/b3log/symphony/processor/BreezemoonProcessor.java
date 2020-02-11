8f97040d45e60a9bf9e05333cf003f761b0e4553
Liang Ding <d@b3log.org>
2020-02-11 20:13:51 +0800

    :recycle: 重构请求路由 #5

--- src/main/java/org/b3log/symphony/processor/BreezemoonProcessor.java
+++ src/main/java/org/b3log/symphony/processor/BreezemoonProcessor.java
@@ -20,15 +20,13 @@ package org.b3log.symphony.processor;
 import org.apache.commons.lang.StringUtils;
 import org.b3log.latke.Keys;
 import org.b3log.latke.Latkes;
-import org.b3log.latke.http.HttpMethod;
+import org.b3log.latke.http.Dispatcher;
 import org.b3log.latke.http.Request;
 import org.b3log.latke.http.RequestContext;
-import org.b3log.latke.http.annotation.After;
-import org.b3log.latke.http.annotation.Before;
-import org.b3log.latke.http.annotation.RequestProcessing;
-import org.b3log.latke.http.annotation.RequestProcessor;
 import org.b3log.latke.http.renderer.AbstractFreeMarkerRenderer;
+import org.b3log.latke.ioc.BeanManager;
 import org.b3log.latke.ioc.Inject;
+import org.b3log.latke.ioc.Singleton;
 import org.b3log.latke.service.LangPropsService;
 import org.b3log.latke.service.ServiceException;
 import org.b3log.latke.util.Paginator;
@@ -36,9 +34,9 @@ import org.b3log.latke.util.Requests;
 import org.b3log.symphony.model.Breezemoon;
 import org.b3log.symphony.model.Common;
 import org.b3log.symphony.model.UserExt;
-import org.b3log.symphony.processor.middleware.*;
-import org.b3log.symphony.processor.middleware.stopwatch.StopwatchEndAdvice;
-import org.b3log.symphony.processor.middleware.stopwatch.StopwatchStartAdvice;
+import org.b3log.symphony.processor.middleware.CSRFMidware;
+import org.b3log.symphony.processor.middleware.LoginCheckMidware;
+import org.b3log.symphony.processor.middleware.PermissionMidware;
 import org.b3log.symphony.service.BreezemoonMgmtService;
 import org.b3log.symphony.service.BreezemoonQueryService;
 import org.b3log.symphony.service.DataModelService;
@@ -57,14 +55,13 @@ import java.util.Map;
  * <li>Adds a breezemoon (/breezemoon), POST</li>
  * <li>Updates a breezemoon (/breezemoon/{id}), PUT</li>
  * <li>Removes a breezemoon (/breezemoon/{id}), DELETE</li>
- * <li>Shows a breezemoon (/breezemoon/{id}), GET</li>
  * </ul>
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.1.2, Jan 5, 2019
+ * @version 2.0.0.0, Feb 11, 2020
  * @since 2.8.0
  */
-@RequestProcessor
+@Singleton
 public class BreezemoonProcessor {
 
     /**
@@ -98,13 +95,26 @@ public class BreezemoonProcessor {
     private LangPropsService langPropsService;
 
     /**
+     * Register request handlers.
+     */
+    public static void register() {
+        final BeanManager beanManager = BeanManager.getInstance();
+        final LoginCheckMidware loginCheck = beanManager.getReference(LoginCheckMidware.class);
+        final PermissionMidware permissionMidware = beanManager.getReference(PermissionMidware.class);
+        final CSRFMidware csrfMidware = beanManager.getReference(CSRFMidware.class);
+
+        final BreezemoonProcessor breezemoonProcessor = beanManager.getReference(BreezemoonProcessor.class);
+        Dispatcher.get("/watch/breezemoons", breezemoonProcessor::showWatchBreezemoon, loginCheck::handle, csrfMidware::fill, permissionMidware::grant);
+        Dispatcher.post("/breezemoon", breezemoonProcessor::addBreezemoon, loginCheck::handle, csrfMidware::check, permissionMidware::check);
+        Dispatcher.put("/breezemoon/{id}", breezemoonProcessor::updateBreezemoon, loginCheck::handle, csrfMidware::check, permissionMidware::check);
+        Dispatcher.delete("/breezemoon/{id}", breezemoonProcessor::removeBreezemoon, loginCheck::handle, csrfMidware::check, permissionMidware::check);
+    }
+
+    /**
      * Shows breezemoon page.
      *
      * @param context the specified context
      */
-    @RequestProcessing(value = "/watch/breezemoons", method = HttpMethod.GET)
-    @Before({StopwatchStartAdvice.class, AnonymousViewCheckMidware.class})
-    @After({CSRFToken.class, PermissionGrant.class, StopwatchEndAdvice.class})
     public void showWatchBreezemoon(final RequestContext context) {
         final Request request = context.getRequest();
 
@@ -154,9 +164,6 @@ public class BreezemoonProcessor {
      *
      * @param context the specified context
      */
-    @RequestProcessing(value = "/breezemoon", method = HttpMethod.POST)
-    @Before({StopwatchStartAdvice.class, LoginCheckMidware.class, CSRFMidware.class, PermissionMidware.class})
-    @After(StopwatchEndAdvice.class)
     public void addBreezemoon(final RequestContext context) {
         context.renderJSON();
 
@@ -204,9 +211,6 @@ public class BreezemoonProcessor {
      *
      * @param context the specified context
      */
-    @RequestProcessing(value = "/breezemoon/{id}", method = HttpMethod.PUT)
-    @Before({StopwatchStartAdvice.class, LoginCheckMidware.class, CSRFMidware.class, PermissionMidware.class})
-    @After(StopwatchEndAdvice.class)
     public void updateBreezemoon(final RequestContext context) {
         final String id = context.pathVar("id");
         context.renderJSON();
@@ -250,9 +254,6 @@ public class BreezemoonProcessor {
      *
      * @param context the specified context
      */
-    @RequestProcessing(value = "/breezemoon/{id}", method = HttpMethod.DELETE)
-    @Before({StopwatchStartAdvice.class, LoginCheckMidware.class, CSRFMidware.class, PermissionMidware.class})
-    @After(StopwatchEndAdvice.class)
     public void removeBreezemoon(final RequestContext context) {
         final String id = context.pathVar("id");
         context.renderJSON();
