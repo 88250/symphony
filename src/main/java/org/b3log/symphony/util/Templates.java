26eb091c5764006c6a615567a5ad7955cc96448c
liuweiqing <liuweiqing147@gmail.com>
2023-11-15 11:08:57 +0800

    修改日志级别

--- src/main/java/org/b3log/symphony/util/Templates.java
+++ src/main/java/org/b3log/symphony/util/Templates.java
@@ -57,25 +57,24 @@ public final class Templates {
         TEMPLATE_CFG.setDefaultEncoding("UTF-8");
         try {
             String path = Templates.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-            LOGGER.info("Code source path: " + path);
-            LOGGER.info("Initial resource path: " + path); // 添加调试信息
+            LOGGER.log(Level.DEBUG, "Code source path: " + path);
             if (StringUtils.contains(path, "/target/classes/") || StringUtils.contains(path, "/target/test-classes/")) {
                 // 开发时使用源码目录
                 path = StringUtils.replace(path, "/target/classes/", "/src/main/resources/");
                 path = StringUtils.replace(path, "/target/test-classes/", "/src/main/resources/");
-                LOGGER.info("Adjusted resource path for development environment: " + path); // 添加调试信息
+                LOGGER.log(Level.DEBUG, "Adjusted resource path for development environment: " + path); // 添加调试信息
             }
             if (StringUtils.contains(path, "/target/symphony/")) {
                 // 开发时使用源码目录
                 path = StringUtils.replace(path, "/target/symphony/", "/src/main/resources/");
-                LOGGER.info("Adjusted resource path for development environment: " + path); // 添加调试信息
+                LOGGER.log(Level.DEBUG, "Adjusted resource path for development environment: " + path); // 添加调试信息
             }
             path += "skins";
             TEMPLATE_CFG.setDirectoryForTemplateLoading(new File(path));
             LOGGER.log(Level.INFO, "Loaded template from directory [" + path + "]");
         } catch (final Exception e) {
             TEMPLATE_CFG.setClassForTemplateLoading(Templates.class, "/skins");
-            LOGGER.error("Failed to load template from directory, loading from classpath", e); // 记录错误信息
+            LOGGER.log(Level.INFO, "Failed to load template from directory, loading from classpath", e); // 记录错误信息
         }
         TEMPLATE_CFG.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         TEMPLATE_CFG.setLogTemplateExceptions(false);
